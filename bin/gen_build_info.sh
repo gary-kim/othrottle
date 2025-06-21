323bb723d31082e5047122747f3001e8d412e0d6
Gary Kim <gary@garykim.dev>
2025-06-21 15:20:54 -0400

    fix: don't fail on not being able to identify build hostname

--- bin/gen_build_info.sh
+++ bin/gen_build_info.sh
@@ -5,7 +5,7 @@ set -euf -o pipefail
 VERSION="git:$(git describe --always --dirty --no-abbrev)"
 GIT_HASH="$(git rev-parse HEAD)"
 COMMIT_DATE="$(git show -s --pretty=%cd --date=format:%Y-%m-%d)"
-BUILD_HOST="$(hash hostname && hostname -f || cat /etc/hostname)"
+BUILD_HOST="$( ( hash hostname && hostname -f ) || cat /etc/hostname || echo "")"
 BUILD_USER="$(whoami)"
 BUILD_TIME="$(date --rfc-3339=seconds)"
 OCAML_VERSION="$(ocamlc --version)"
